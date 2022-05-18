5574b118b131b6c235f3aabb1560cc73658cac1b
TheRaynex <rafael.luedtke@web.de>
2022-05-18 14:15:35 +0200

    number display to show both numbers

--- WackJackDis.asm
+++ WackJackDis.asm
@@ -1,40 +1,102 @@
 ;Stellen Port = p2
-; bit 0 v Ziffer 0, 1 fuer 1, 2 fuer 2 und 3 fuer 3
+;bit 0 v Ziffer 0, 1 fuer 1, 2 fuer 2 und 3 fuer 3
 ;Port p3 um Ziffer zu zeigen
 ; A = bit 0, B = bit 1, usw...
-start: 
 
-;Stelle 0 ansprechen
-Mov p2, #00000001b
+;Zahl der Bank in r2 
+;Zahl des Spielers in r3
 
-; Ziffer 3 aus db holen und in anzeige port schreiben
+;-------------------------------------
+;  Disassembles the Numbers of the Bank and the Player 
+;  and displays them in the LED Display
+;-------------------------------------
+displayNumbers:
+;Beispiel Zahlen
+mov r2, #8
+mov r3, #20
+
+;Disassembly Number of Bank
+mov a, r2
+mov b,#10
+div ab
+;Display Number of Bank
+call disPositionZB
+call disPositionEB
+
+;Display Point as separation
+call disPoint
+
+;Disassembly Number of Player
+mov a, r3
+mov b,#10
+div ab
+;Display Number of Player
+call disPositionZS
+call disPositionES
+
+jmp displaynumbers
+;-------------------------------------
+
+;-------------------------------------
+;  Display functions
+;-------------------------------------
+;Zehnerstelle Bank
+disPositionZB:
+setb p2.3
 mov DPTR, #table
-mov a, #3
 movc a,@a+dptr
-mov r3, a
-mov p3, r3
+mov p3, a
 
 call setPort
+ret
 
-;Stelle 1 ansprechen
-Mov p2, #00000010b
-; Ziffer 1 aus db holen und in anzeige port schreiben
+;Einerstelle Bank
+disPositionEB:
+setb p2.2
 mov DPTR, #table
-mov a, #1
+mov a, b
 movc a,@a+dptr
-mov r3, a
-mov p3, r3
+mov p3, a
 
 call setPort
+ret
 
-jmp start
+;Zehnerstelle Spieler
+disPositionZS:
+setb p2.1
+mov DPTR, #table
+movc a,@a+dptr
+mov p3, a
 
+call setPort
+ret
+
+;Einerstelle Spieler
+disPositionES:
+setb p2.0
+mov DPTR, #table
+mov a, b
+movc a,@a+dptr
+mov p3, a
+
+call setPort
+ret
+
+;Punkt zum trennen der Bank und des Spielers
+disPoint:
+setb p2.2
+mov p3, #10000000b
+call setPort
+ret
+
+;-------------------------------------
 ;Reset ports to switch segment
 setPort:
 mov p3, #00000000b
 mov p2, #00000000b
 ret
 
+;-------------------------------------
 table:
 db 00111111b ;0
 db 00000110b, 01011011b, 01001111b ;1, 2, 3
